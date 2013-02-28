d38eb4ebbf402fc9f52bee40e2771909aa125265
Jan-Wijbrand Kolman <none@none>
2013-02-28 16:54:43 +0100

    setup a grokcore.error package structure

---
+++ README.txt
@@ -0,0 +1,12 @@
+grokcore.error
+**************
+
+This package provides an configurable error reporting utility that emit
+exception information as log entries.
+
+.. contents::
+
+Setting up ``grokcore.error``
+===============================
+
+...
---
+++ setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read(os.path.join('src', 'grokcore', 'error', 'README.txt'))
+    + '\n' +
+    read('CHANGES.txt')
+    )
+
+tests_require = [
+    'zope.app.appsetup',
+    'zope.component',
+    'zope.interface',
+    'zope.testing',
+    'zope.security',
+    'zope.securitypolicy',
+    ]
+
+debug_requires = [
+    'IPython',
+    ]
+
+setup(
+    name='grokcore.error',
+    version='1.0.dev0',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://pypi.python.org/pypi/grokcore.startup',
+    description='Paster support for Grok projects.',
+    long_description=long_description,
+    license='ZPL',
+    keywords='zope zope3 grok grokproject WSGI Paste paster',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Framework :: Zope3',
+        ],
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['grokcore'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.component',
+        'zope.publisher',
+        'zope.dottedname',
+        'zope.app.wsgi',
+        'zope.app.debug',
+        ],
+    tests_require = tests_require,
+    extras_require = dict(test=tests_require, debug=debug_requires),
+    },
+)
