8cee213b76d317046decfc07f0251dc62f13e775
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-16 12:15:48 +0100

    Preparing release 3.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if sys.version_info.major == 2:
 
 setup(
     name='grokcore.error',
-    version='3.0.1.dev0',
+    version='3.0.1',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
